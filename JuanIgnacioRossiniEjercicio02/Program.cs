ddd61eeecbef26a328ab4bfe6882a2f5def7b38e
Juan Ignacio Rossini <koki-rossini@hotmail.com.ar>
2021-07-12 19:47:27 -0300

    Agregar archivos de proyecto.

---
+++ JuanIgnacioRossiniEjercicio02/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JuanIgnacioRossiniEjercicio02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int lado1, lado2, lado3;
+            string tipoTriangulo;
+
+            Console.WriteLine("ingrese el primer lado");
+            lado1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("ingrese el segundo lado");
+            lado2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("ingrese el tercer lado");
+            lado3 = Convert.ToInt32(Console.ReadLine());
+
+            if (lado1+lado2>lado3 && lado2+lado3>lado1 && lado1+lado3>lado2)
+            {
+                if (lado1==lado2 && lado2==lado3 && lado1==lado3)
+                {
+                    tipoTriangulo = "equilatero";
+                    Console.WriteLine($"las medidas ingresadas forman un triangulo:{tipoTriangulo}");
+                }
+
+                if (lado1!=lado2 && lado2!=lado3 && lado1!=lado3)
+                {
+                    tipoTriangulo = "escaleno";
+                    Console.WriteLine($"las medidas ingresadas forman un triangulo:{tipoTriangulo}");
+                }
+
+                if (lado1==lado2 && lado1!=lado3 || lado2==lado3 && lado2!=lado1 || lado1==lado3 && lado1!=lado2)
+                {
+                    tipoTriangulo = "isosceles";
+                    Console.WriteLine($"las medidas ingresadas forman un triangulo:{tipoTriangulo}");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("las medidas ingresadas no forman un triangulo");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
